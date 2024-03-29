a0983cf37ca3aebe849ece8c6ee9e182b00ab15d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-29 21:19:29 +0800

    feat: make skeletal widget of results_screen
     On branch working
     Changes to be committed:
    	modified:   lib/results_screen.dart

---
+++ lib/results_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text
+            const Text('You Answer X out of Y questions correctly !!'),
+            // Padding SizedBox
+            const SizedBox(height: 30,),
+            // Scrolling panel
+            // Text with icon widget inside scrolling panel
+            const Text('List of answer and questions..'),
+            const SizedBox(height: 30,),
+            // button for restart quiz
+            TextButton(onPressed: () {}, child: const Text('Restart Quiz!')),
+          ],
+        ),
+      ),
+    );
+  }
+}
