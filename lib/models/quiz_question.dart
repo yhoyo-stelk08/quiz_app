bbfe602b23f1944fe5efebb1ea486f490e2e98a8
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-29 10:53:16 +0800

    make new method to shuffle the answer

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion(this.question, this.answers);
+
+  List<String> getShuffledAnswer() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
