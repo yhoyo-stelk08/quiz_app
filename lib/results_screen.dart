04556895517e03a23e4e50bdf773bab4781b1eb3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-29 22:09:07 +0800

    feat: add function for get summary data
     On branch working
     Changes to be committed:
    	modified:   lib/results_screen.dart

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.choosenAnswer});
 
   final List<String> choosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    List<Map<String, Object>> summary = [];
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].question,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': choosenAnswer[i],
+        },
+      );
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +33,15 @@ class ResultScreen extends StatelessWidget {
             // Text
             const Text('You Answer X out of Y questions correctly !!'),
             // Padding SizedBox
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             // Scrolling panel
             // Text with icon widget inside scrolling panel
             const Text('List of answer and questions..'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             // button for restart quiz
             TextButton(onPressed: () {}, child: const Text('Restart Quiz!')),
           ],
