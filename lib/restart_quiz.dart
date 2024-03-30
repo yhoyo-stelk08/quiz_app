163ee3e4e8f788ac9cbda499796254ead294f050
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-30 23:11:15 +0800

    feat: adding new class restart quiz for the button restart quiz.

---
+++ lib/restart_quiz.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RestartQuiz extends StatelessWidget {
+  const RestartQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text('Restart Quiz'),
+    );
+  }
+}
